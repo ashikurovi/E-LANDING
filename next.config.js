0f84ef6936699396fbbf24089aa1a3d345e84200
Ashikur Rahman Ovi <ashikurovi2003@gmail.com>
2026-02-18 18:11:48 +0600

    update

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
       { protocol: "https", hostname: "res.cloudinary.com" },
       { protocol: "https", hostname: "i.ibb.co" },
       { protocol: "https", hostname: "squadlog-cdn.up.railway.app", pathname: "/uploads/**" },
-      { protocol: "http", hostname: "squadlog-cdn.up.railway.app", pathname: "/uploads/**" },
+      { protocol: "https", hostname: "squadlog-cdn.up.railway.app", pathname: "/uploads/**" },
     ],
   },
   webpack: (config) => {
